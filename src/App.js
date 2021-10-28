67c262de14404801ea3e4166696ee3edab02a4f1
Hemanth Mudra <mudra.hemanth@gmail.com>
2021-10-28 15:20:26 +0530

    integrated linecharts

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import {
+   Navbar, Cryptocurrencies, CryptoDetails, Exchanges, Homepage
+   , News
+} from './components';
+
+const app = () => {
+   return (
+         <div className='app'>
+            <div className='navbar'>
+               <Navbar />
+            </div>
+            <div className='main'>
+               <Layout>
+                  <div className="routes">
+                     <Switch>
+                        <Route exact path="/" >
+                           <Homepage />
+                        </Route>
+                        <Route exact path="/cryptocurrencies" >
+                           <Cryptocurrencies />
+                        </Route>
+                        <Route exact path="/crypto/:coinID" >
+                           <CryptoDetails />
+                        </Route>
+                        <Route exact path="/exchanges" >
+                           <Exchanges />
+                        </Route>
+                        <Route exact path="/news" >
+                           <News />
+                        </Route>
+                     </Switch>
+                  </div>
+               </Layout>
+
+               <div className='footer'>
+                  <Typography.Title level={5} style={{ color: 'white', textAlign: 'Center' }}>
+                     Cryptoverse <br />
+                     All rights reserved
+                  </Typography.Title>
+                  <Space>
+                     <Link to='/'>Home</Link>
+                     <Link to='/exchanges'>Exchanges</Link>
+                     <Link to='/news'>News</Link>
+                  </Space>
+               </div>
+            </div>
+         </div>
+   )
+}
+
+export default app
