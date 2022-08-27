f9b16e90b19f6805b2b15da5d0581ab4181f4982
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2022-08-27 13:39:41 +0530

    Update cryptoAPI.js

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,8 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-   'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-   'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
+   'x-access-token': process.env.REACT_APP_RAPIDAPI_KEY,
 }
 
 const baseUrl = process.env.REACT_APP_CRYPTO_RAPIDAPI_URL;
