e781f6f07b1854c3d442f986fcd9eb808f6ab890
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2022-08-27 19:09:52 +0530

    added proxy backend for cors

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,49 +1,39 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-  "x-access-token": process.env.REACT_APP_RAPIDAPI_KEY,
-};
+   'x-access-token': process.env.REACT_APP_RAPIDAPI_KEY,
+}
 
 const baseUrl = process.env.REACT_APP_CRYPTO_RAPIDAPI_URL;
 
-const createRequest = (url, params) => ({
-  url,
-  headers: cryptoApiHeaders,
-  params: { "x-access-token": process.env.REACT_APP_RAPIDAPI_KEY, ...params },
-});
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
 
-const buildParams = ({ params }) => {
-  let queryString = URLSearchParams({
-    "x-access-token": process.env.REACT_APP_RAPIDAPI_KEY,
-    ...params,
-  });
-  console.log(queryString);
-  return queryString;
-};
+const buildParams = ({params}) => {
+   let queryString =  URLSearchParams({
+   'x-access-token': process.env.REACT_APP_RAPIDAPI_KEY,
+   ...params
+   })
+   console.log(queryString)
+}
+
 
 export const cryptoApi = createApi({
-  reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl }),
-  endpoints: (builder) => ({
-    getCryptos: builder.query({
-      query: (count) => createRequest(`/coins`, { limit: count }),
-    }),
-    getCryptoDetails: builder.query({
-      query: (coinID) => createRequest(`/coin/${coinID}`),
-    }),
-    getCryptoHistory: builder.query({
-      query: ({ coinID, timePeriod }) =>
-        createRequest(`/coin/${coinID}/history/?timePeriod=${timePeriod}`),
-    }),
-    getExchanges: builder.query({
-      query: () => createRequest(`/exchanges`),
-    }),
-  }),
-});
+   reducerPath: 'cryptoApi',
+   baseQuery: fetchBaseQuery({baseUrl}),
+   endpoints: (builder) => ({
+      getCryptos: builder.query({
+         query : (count) => createRequest(`/coins?limit=${count}`)
+      }),
+      getCryptoDetails: builder.query({
+         query : (coinID) => createRequest(`/coin/${coinID}`),
+      }),
+      getCryptoHistory: builder.query({
+         query : ({coinID, timePeriod}) => createRequest(`/coin/${coinID}/history/?timePeriod=${timePeriod}`),
+      }),
+      getExchanges : builder.query({
+         query : () => createRequest(`/exchanges`),
+      }),
+   })
+})
 
-export const {
-  useGetCryptosQuery,
-  useGetCryptoDetailsQuery,
-  useGetCryptoHistoryQuery,
-  useGetExchangesQuery,
-} = cryptoApi;
+export const {useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery} = cryptoApi;
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 const cryptoNewsHeaders = {
    'x-bingapis-sdk': 'true',
    'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
-   'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+   'x-rapidapi-key': process.env.REACT_APP_NEWS_API_KEY,
  }
 
  const baseUrl = process.env.REACT_APP_NEWS_API_URL;
